b109de415a80ee6568bcb6d0bc1474a24d39afbe
Ulises <cruu_2005@hotmail.com>
2020-01-23 12:53:20 -0700

    se crea imagen

---
+++ README.md
@@ -0,0 +1,8 @@
+# TapLayout en Android
+
+
+Ejemplo de uso de TapLayout en Android
+
+
+
+![](tap.png)
---
+++ app/src/main/java/com/example/taplayout/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.taplayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
